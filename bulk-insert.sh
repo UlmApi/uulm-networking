9496bd6e3ce1306114fa4729d2b0f2a1768e92a7
cmichi <mich@elmueller.net>
2013-02-23 17:32:31 +0100

    committing bulk-insert script

--- bulk-insert.sh
+++ bulk-insert.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
+# This script parses the provided network data, 
+# creates native JS-objects from it and posts the
+# data to a CouchDB database.
+
+node parser.js
+node post\-apos.js > parsing/aps.json
 
 for i in ./parsing/*; do
 	curl --header 'content-type: application/json' -X POST http://localhost:5984/uulm-networking/_bulk_docs -d @"$i"
 done
+
+curl --header 'content-type: application/json' -vX POST http://localhost:5984/uulm-networking/_bulk_docs -d @./parsing/aps.json
+
+
+
 
 
