39056ddc015885d89bd82c9b34500e646b9ef91a
cmichi <mich@elmueller.net>
2013-02-23 14:48:56 +0100

    starting with parser, basic webstuff is working

---
+++ server.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+var express = require('express');
+var io = require('socket.io');
+
+var app = express();
+app.use(express.static(__dirname + '/static'));
+app.use(express.bodyParser());
+
+var server = require('http').createServer(app);
+io = io.listen(server);
+
+
+
+// parse file stuff
+
+
+io.sockets.on('connection', function (socket) {
+	socket.emit('connection');
+
+	socket.on('pull', function (data){
+		socket.emit('push', [{
+			day: "Fri", 
+			name: "Fri", 
+			lat: 48.40783887047417,
+			lon: 9.987516403198242
+		}]);
+	});
+});
+
+
+server.listen(process.env.PORT || 3000, function() {
+	console.log('Listening on port ' + server.address().port);
+});
