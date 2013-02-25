1c42619f6cbb6351f997c86909ae1cdc15fa2b89
cmichi <mich@elmueller.net>
2013-02-25 15:09:51 +0100

    working state couchapp

--- couchapp/_attachments/js/show.js
+++ couchapp/_attachments/js/show.js
@@ -1,64 +1,59 @@
 var map;
-var entity_groups = [];
-var tile_groups = [];
-var info, ctrls, legend;
-var initialized = false;
-var prefs = {};
-var prefs_dropped = {};
-var socket;
-var now = new Date();
-
 var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/1443dfdd3c784060aedbf4063cd1709b/997/256/{z}/{x}/{y}.png';
 var cloudmadeAttribution = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade';
 
-var dialog_opt = {
-	resizable: false
-	, width: 550
-	, modal: true
-}
+var curr_doc;
+var leftCount = 0;
 
 
-document.addEventListener('DOMContentLoaded', function() {
+$(function() {
 	map = L.map('map', {
 		center: new L.LatLng(48.422650593077435, 9.954675436019896)
 		, zoom: 17
-		, layers: tile_groups
 	});
 	map.on('click', onMapClick);
 	L.tileLayer(cloudmadeUrl, {attribution: cloudmadeAttribution}).addTo(map);
 
-
 	$.couch.urlPrefix = "http://localhost:5984";
-	$.couch.info({
-	    success: function(data) {
-		    console.log(data);
-		}
-	});
+	fetchNext();
+}, false);
 
-	// get all aps from view
+
+function fetchNext() {
 	$.couch.db("uulm-networking").view("couchapp/aps", {
 		success: function(data) {
-			// console.log(data);
-			take(data.rows[0]);
+			// count how many aps with zero entries are left
+			/*
+			leftCount = 0;
+			for (var i in data.rows) {
+				console.log(data.rows[i])
+				if (data.rows[i].coords.length === 0)
+					leftCount++;
+			}
+			*/
+
+			displayAP(data.rows[0]);
 		},
 		error: function(status) {
 			console.log(status);
 		},
 		reduce: false
 	});
+}
 
-}, false);
 
-
-var curr_doc;
-function take(data) {
+function displayAP(data) {
 	var id = data.id;
-	console.log(id);
-	console.log(data);
+	//console.log(id);
+	//console.log(data);
 
 	$.couch.db("uulm-networking").openDoc(id, {
 		success: function(data) {
 			curr_doc = data;
+			console.log(curr_doc)
+			$("#ap_id").text(curr_doc._id);
+			$("#ap_desc").text(curr_doc.desc);
+			$("#aps_left").text(leftCount);
 		},
 		error: function(status) {
 			console.log(status);
@@ -71,18 +66,18 @@ function onMapClick(e) {
 	var coord = [e.latlng.lat, e.latlng.lng];
 	if (curr_doc) {
 		curr_doc.coords.push(coord);
-		save(curr_doc);
+		saveAP(curr_doc);
 	}
 }
 
 
-function save(doc) {
+function saveAP(doc) {
 	console.log(doc);
 	$.couch.db("uulm-networking").saveDoc(doc, {
 		success: function(data) {
 			console.log(data);
 			curr_doc = undefined;
-			// fetch new entry
+			fetchNext();
 		},
 		error: function(status) {
 			console.log(status);
