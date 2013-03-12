19c6339d898394932122bfb3380e93d88ea65852
cmichi <mich@elmueller.net>
2013-03-12 19:38:26 +0100

    particles showing up and rotating is working

--- visualization/js/show.js
+++ visualization/js/show.js
@@ -14,6 +14,8 @@ var FizzyText = function() {
   this.weekday = "Monday";
 };
 
+var clock = new THREE.Clock();
+var keyboard = new THREEx.KeyboardState();
 
 $(function() {
 	var text = new FizzyText();
@@ -28,134 +30,25 @@ $(function() {
 	animate();
 });
 
-
-function init() {
-	container = document.createElement( 'div' );
-	document.body.appendChild( container );
-	scene = new THREE.Scene();
-	scene.fog = new THREE.FogExp2( 0x000000, 0.0009 );
-
-
-	var VIEW_ANGLE = 35, 
-		ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, 
-		NEAR = 1, FAR = 5000;
-	camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
-
-	camera.position.set(400, -1307, 2352)
-	camera.up.set(-0.858, 1.62, 1.32)
-	camera.rotation.set(0.5, 0.304, 0.424)
-	scene.add(camera);
-
-	THREEx.WindowResize(renderer, camera);
-	THREEx.FullScreen.bindKey({ charCode : 'm'.charCodeAt(0) });
-
-	var light = new THREE.PointLight(0xffffff);
-	light.position.set(0,0,1800);
-	scene.add(light);
-
-var ambient = new THREE.AmbientLight( 0xFFFFFF );
-scene.add(ambient);
-
-	var directionalLight = new THREE.DirectionalLight(0xffffff);
-	directionalLight.position.set(1, 1, 1).normalize();
-	scene.add(directionalLight);
-
-	/* floor */
-	var material = new THREE.MeshBasicMaterial({
-		map: THREE.ImageUtils.loadTexture("map-simple.svg"),
-fog: false
-	});
-
-	var geometry = new THREE.PlaneGeometry(3074, 1782);
-	var meshCanvas = new THREE.Mesh(geometry, material);
-	scene.add(meshCanvas);
-
-
-
-
-
-
-
-
-
-
-///////////////////////////////////
-
-
-
-				geometry = new THREE.Geometry();
-
-				sprite = THREE.ImageUtils.loadTexture( "ball.png" );
-
-				for ( i = 0; i < 5000; i ++ ) {
-
-					var vertex = new THREE.Vector3();
-					vertex.x = 2000 * Math.random() - 1000;
-					vertex.y = 2000 * Math.random() - 1000;
-					vertex.z = 2000 * Math.random() - 1000;
-
-					geometry.vertices.push( vertex );
-
-					colors[ i ] = new THREE.Color( 0xffffff );
-					colors[ i ].setHSV( ( vertex.x + 1000 ) / 2000, 1, 1 );
-
-				}
-
-				geometry.colors = colors;
-
-				material = new THREE.ParticleBasicMaterial( { size: 185, map: sprite, vertexColors: true, transparent: true } );
-				material.color.setHSV( 1.0, 0.2, 0.8 );
-
-				particles = new THREE.ParticleSystem( geometry, material );
-				particles.sortParticles = true;
-
-				scene.add( particles );
-
-
-
-
-
-
-
-
-
-//////////////////////////////
-				var material2 = new THREE.ParticleBasicMaterial( { map: new THREE.Texture( generateSprite() ), 
-fog: false,
-blending: THREE.AdditiveBlending } );
-
-
-var sphereMaterial = new THREE.MeshLambertMaterial(
-	{color: 0xFF0000, 
-	//transparent: true,
-	blending: THREE.AdditiveBlending
-});
-
-
-				for ( var i = 0; i < 1000; i++ ) {
-
-					particle = new THREE.Particle( material2 );
-//var sphere = new THREE.Mesh(new THREE.SphereGeometry( 300, 8, 8), sphereMaterial);
-					initParticle( particle, i * 10 );
-
-					scene.add( particle );
-				}
-
-
+function pGroup(x, y, h) {
 	var particleTexture = THREE.ImageUtils.loadTexture( 'spark.png' );
+	h *= 3;
 
 	particleGroup = new THREE.Object3D();
 	particleAttributes = { startSize: [], startPosition: [], randomness: [] };
 	
 	var totalParticles = 200;
-	var radiusRange = 50;
+	var radiusRange = 50 * h;
 	for( var i = 0; i < totalParticles; i++ ) 
 	{
-	    var spriteMaterial = new THREE.SpriteMaterial( { map: particleTexture, fog: true, useScreenCoordinates: false, color: 0xffffff } );
+	    var spriteMaterial = new THREE.SpriteMaterial( { map: particleTexture, useScreenCoordinates: false, color: 0xffffff } );
 		
 		var sprite = new THREE.Sprite( spriteMaterial );
-		sprite.scale.set( 32, 32, 1.0 ); // imageWidth, imageHeight
+		sprite.scale.set( h*32, h*32, 1.0 ); // imageWidth, imageHeight
+		//sprite.scale.set( 32, 32, 1.0 ); // imageWidth, imageHeight
+		//sprite.position.set( Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5 );
 		sprite.position.set( Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5 );
+
 		// for a cube:
 		// sprite.position.multiplyScalar( radiusRange );
 		// for a solid sphere:
@@ -174,15 +67,55 @@ var sphereMaterial = new THREE.MeshLambertMaterial(
 		particleAttributes.startPosition.push( sprite.position.clone() );
 		particleAttributes.randomness.push( Math.random() );
 	}
-	particleGroup.position.y = 50;
-//	scene.add(particleGroup );
+	particleGroup.position.x = x;
+	particleGroup.position.y = y;
+	scene.add( particleGroup );
+}
 
-/*
-sphere.position.x = 0;
-sphere.position.y = 0;
-sphere.position.z = 0;
-*/
-//scene.add(sphere)
+
+function init() {
+	scene = new THREE.Scene();
+//	scene.fog = new THREE.FogExp2( 0x000000, 0.0009 );
+
+	var VIEW_ANGLE = 35, 
+		ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, 
+		NEAR = 1, FAR = 5000;
+	camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+
+	camera.position.set(400, -1307, 2352)
+	camera.up.set(-0.858, 1.62, 1.32)
+	camera.rotation.set(0.5, 0.304, 0.424)
+	scene.add(camera);
+
+
+	renderer = new THREE.WebGLRenderer({ antialias: false,  clearAlpha: 1 });
+	renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+	//renderer.setClearColor("#fff", 1);
+container = document.createElement( 'div' );
+	document.body.appendChild( container );
+	container.appendChild(renderer.domElement);
+
+	controls = new THREE.TrackballControls(camera, renderer.domElement);
+
+	THREEx.WindowResize(renderer, camera);
+	THREEx.FullScreen.bindKey({ charCode : 'm'.charCodeAt(0) });
+
+
+var light = new THREE.PointLight(0xffffff);
+	light.position.set(0,250,0);
+	scene.add(light);
+
+
+	/* floor */
+	var material = new THREE.MeshBasicMaterial({
+		map: THREE.ImageUtils.loadTexture("map-simple.svg"),
+fog: false
+	});
+
+	var geometry = new THREE.PlaneGeometry(3074, 1782);
+	var meshCanvas = new THREE.Mesh(geometry, material);
+	scene.add(meshCanvas);
+
 
 
 /*
@@ -204,67 +137,62 @@ sphere.position.z = 0;
 	displayEntireWeek();
 	//exampleSphere();
 
-
-
-
-	renderer = new THREE.WebGLRenderer({ antialias: false,  clearAlpha: 1 });
-	renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-	//renderer.setClearColor("#fff", 1);
-
-	container.appendChild(renderer.domElement);
-
-	controls = new THREE.TrackballControls(camera, renderer.domElement);
 }
 
 
 
-			function generateSprite() {
+var particleGroup, particleAttributes;
 
-				var canvas = document.createElement( 'canvas' );
-				canvas.width = 16;
-				canvas.height = 16;
 
-				var context = canvas.getContext( '2d' );
-				var gradient = context.createRadialGradient( canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2 );
-				gradient.addColorStop( 0, 'rgba(255,255,255,1)' );
-				gradient.addColorStop( 0.2, 'rgba(0,255,255,1)' );
-				gradient.addColorStop( 0.4, 'rgba(0,0,64,1)' );
-				gradient.addColorStop( 1, 'rgba(0,0,0,1)' );
+function animate() 
+{
+    requestAnimationFrame( animate );
+	render();		
+	update();
+}
 
-				context.fillStyle = gradient;
-				context.fillRect( 0, 0, canvas.width, canvas.height );
+function update()
+{
+	var time = 4 * clock.getElapsedTime();
+	
+	for ( var c = 0; c < particleGroup.children.length; c ++ ) 
+	{
+		var sprite = particleGroup.children[ c ];
 
-				return canvas;
+		// particle wiggle
+		// var wiggleScale = 2;
+		// sprite.position.x += wiggleScale * (Math.random() - 0.5);
+		// sprite.position.y += wiggleScale * (Math.random() - 0.5);
+		// sprite.position.z += wiggleScale * (Math.random() - 0.5);
+		
+		// pulse away/towards center
+		// individual rates of movement
+		var a = particleAttributes.randomness[c] + 1;
+		var pulseFactor = Math.sin(a * time) * 0.1 + 0.9;
+		sprite.position.x = particleAttributes.startPosition[c].x * pulseFactor;
+		sprite.position.y = particleAttributes.startPosition[c].y * pulseFactor;
+		sprite.position.z = particleAttributes.startPosition[c].z * pulseFactor;	
+	}
 
-			}
+	// rotate the entire group
+	// particleGroup.rotation.x = time * 0.5;
+	particleGroup.rotation.y = time * 0.75;
+	// particleGroup.rotation.z = time * 1.0;
 
-			function initParticle( particle, delay ) {
+	if ( keyboard.pressed("z") ) 
+	{ 
+		// do something cool
+	}
+	
+	controls.update();
+	
+}
 
-				var particle = this instanceof THREE.Particle ? this : particle;
-				var delay = delay !== undefined ? delay : 0;
+function render() 
+{
+	renderer.render( scene, camera );
+}
 
-				particle.position.x = 0;
-				particle.position.y = 0;
-				particle.position.z = 0;
-				particle.scale.x = particle.scale.y = Math.random() * 30 + 1;
-
-				new TWEEN.Tween( particle )
-					.delay( delay )
-					.to( {}, 10000 )
-					.onComplete( initParticle )
-					.start();
-
-				new TWEEN.Tween( particle.position )
-					.delay( delay )
-					.to( { x: Math.random() * 4000 - 2000, y: Math.random() * 1000 - 500, z: Math.random() * 4000 - 2000 }, 10000 )
-					.start();
-
-				new TWEEN.Tween( particle.scale )
-					.delay( delay )
-					.to( { x: 0, y: 0 }, 10000 )
-					.start();
-
-			}
 
 function exampleSphere() {
 	//var sphere = new THREE.Mesh(new THREE.SphereGeometry(60, 50, 50), material);
@@ -320,59 +248,27 @@ function displayEntireWeek() {
 		var pos = coord2px(value[0][0], value[0][1]);
 		sphere.position.x = pos.x;
 		sphere.position.y = pos.y;
-		scene.add(sphere);
+		//scene.add(sphere);
+		pGroup(pos.x, pos.y, h);
 
+		if (i == 1) break;
 		//break;
 	}
 }
 
-
-function animate() {
-	requestAnimationFrame(animate);
-	render();
-}
-
-
-function render() {
-	//camera.position.x += 10;
-
-	//renderer.clear();
 /*
-	var time = Date.now() * 0.00005;
-
-				
-				h = ( 360 * ( 1.0 + time ) % 360 ) / 360;
-				material.color.setHSV( h, 0.8, 1.0 );
-
-TWEEN.update();
-
-
-				for ( i = 0; i < scene.children.length; i ++ ) {
-
-					var object = scene.children[ i ];
-
-					if ( object instanceof THREE.ParticleSystem ) {
-
-						object.rotation.y = time * ( i < 4 ? i + 1 : - ( i + 1 ) );
-
-					}
-
-				}
-*/
-	
-
-
+function render() {
 	renderer.render(scene, camera);
 	controls.update();
-	/*
+	
 	console.log(
 		" " + camera.position.x +
 		" " + camera.position.y +
 		" " + camera.position.z
 	);
-	*/
-}
 
+}
+*/
 function coord2px(lat, lon) {
 	/*
 		48.42677
@@ -399,5 +295,4 @@ function coord2px(lat, lon) {
 
 	return {x: coordX, y: coordY};
 }
-
 
